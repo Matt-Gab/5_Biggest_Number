0b28e789e12d3ef3ff638231d7bd7c445e5c9a18
Matt-Gab <matthewe.egabriel@gmail.com>
2024-10-14 12:16:02 +0800

    first iteration

---
+++ Biggest_5_Nums.py
@@ -0,0 +1,22 @@
+First_Number = input()
+Second_Number = input()
+Third_Number = input()
+Fourth_Number = input()
+Fifth_Number = input()
+
+if First_Number + Second_Number > Third_Number + Fourth_Number:
+    if First_Number - Second_Number >= 0:
+        if First_Number > Fifth_Number:
+            print(First_Number)
+        print(Fifth_Number)
+    if Second_Number > Fifth_Number:
+        print(Second_Number)
+    print(Fifth_Number)
+if Third_Number -- Fourth_Number >= 0:
+    if Third_Number > Fifth_Number:
+        print(Third_Number)
+    print(Fifth_Number)
+if Fourth_Number > Fifth_Number:
+    print(Fourth_Number)
+print(Fifth_Number)
+    
