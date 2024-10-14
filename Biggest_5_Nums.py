0286d750d10d29e48d41507ab7482b4bf7503e04
Matt-Gab <matthewe.egabriel@gmail.com>
2024-10-14 13:23:11 +0800

    first working iteration

--- Biggest_5_Nums.py
+++ Biggest_5_Nums.py
@@ -1,22 +1,31 @@
-First_Number = input()
-Second_Number = input()
-Third_Number = input()
-Fourth_Number = input()
-Fifth_Number = input()
+First_Number = int(input('First Number: '))
+Second_Number = int(input('Second Number: '))
+Third_Number = int(input('Third Number: '))
+Fourth_Number = int(input('Fourth Number: '))
+Fifth_Number = int(input('Fifth Number: '))
 
-if First_Number + Second_Number > Third_Number + Fourth_Number:
-    if First_Number - Second_Number >= 0:
-        if First_Number > Fifth_Number:
-            print(First_Number)
-        print(Fifth_Number)
-    if Second_Number > Fifth_Number:
-        print(Second_Number)
-    print(Fifth_Number)
-if Third_Number -- Fourth_Number >= 0:
-    if Third_Number > Fifth_Number:
-        print(Third_Number)
-    print(Fifth_Number)
-if Fourth_Number > Fifth_Number:
-    print(Fourth_Number)
-print(Fifth_Number)
-    
+if First_Number - Second_Number >= 0:
+    if First_Number - Third_Number >= 0:
+        if First_Number - Fourth_Number >= 0:
+            if First_Number - Fifth_Number >= 0:
+                print('The Largest is the First Number', First_Number)
+if Second_Number - First_Number >= 0:
+    if Second_Number - Third_Number >= 0:
+        if Second_Number - Fourth_Number >= 0:
+            if Second_Number - Fifth_Number >= 0:
+                print('The Largest is the Second Number', Second_Number)
+if Third_Number - First_Number >= 0:
+    if Third_Number - Second_Number >= 0:
+        if Third_Number - Fourth_Number >= 0:
+            if Third_Number - Fifth_Number >= 0:
+                print('The Largest is the Third Number', Third_Number)
+if Fourth_Number - First_Number >= 0:
+    if Fourth_Number - Second_Number >= 0:
+        if Fourth_Number - Third_Number >= 0:
+            if Fourth_Number - Fifth_Number >= 0:
+                print('The Largest is the Fourth Number', Fourth_Number)
+if Fifth_Number - First_Number >= 0:
+    if Fifth_Number - Second_Number >= 0:
+        if Fifth_Number - Third_Number >= 0:
+            if Fifth_Number - Fourth_Number >= 0:
+                print('The Largest is the Fifth Number', Fifth_Number)
